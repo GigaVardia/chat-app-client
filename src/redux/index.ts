b86033a08045bdbab2714a95835b0c9fac08c56c
GigaVardia42 <giga.vardia@lavapi.com>
2021-08-05 00:39:48 +0400

    added redux store

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,1 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./reducers";
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
