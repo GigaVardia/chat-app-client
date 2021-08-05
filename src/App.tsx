a6aca67258751797d83cab957601cfac66c210d1
GigaVardia42 <giga.vardia@lavapi.com>
2021-08-05 23:57:37 +0400

    added router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import React, { FC } from 'react';
 import { Home } from './components/Home';
 import { lightTheme, darkTheme } from './styles/theme';
 import { useTypedSelector } from './hooks/useTypedSelector';
+import { Route, Switch } from 'react-router-dom';
 
 const App:FC = () => {
   const { theme } = useTypedSelector(state => state.theme)
 
   return (
     <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-      <React.Fragment>
         <CssBaseline />
-        <Home/>
-      </React.Fragment>
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+        </Switch>
     </ThemeProvider>
   );
 }
